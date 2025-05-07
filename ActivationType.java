fd90c4758e7bb38306f0df832aa2c853ee6aeeca
Showmick Das <showmickdas75@gmail.com>
2025-05-07 19:19:09 -0400

    Create ActivationType.java

---
+++ ActivationType.java
@@ -0,0 +1,12 @@
+/**
+ * Enums which represent the different activation functions our
+ * single-layer perceptron model can use. Our given options are
+ * the Rectified Linear Unit (ReLU), Sigmoid and Exponential
+ * Linear Unit (ELU).
+ * @author Showmick Das
+ * @version 1.0
+ */
+
+public enum ActivationType {
+    RELU, SIGMOID, ELU
+}
